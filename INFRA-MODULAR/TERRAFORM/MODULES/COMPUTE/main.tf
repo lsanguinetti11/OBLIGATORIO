1ad6970c953736cd994dd167a2f0faff7d6acaed
Luis Sanguinetti <luisurco8@gmail.com>
2025-11-15 11:15:06 -0300

    Se define SG, se agrega ingress y egress para hhttp y ssh

--- INFRA-MODULAR/TERRAFORM/MODULES/COMPUTE/main.tf
+++ INFRA-MODULAR/TERRAFORM/MODULES/COMPUTE/main.tf
@@ -1,4 +1,32 @@
-
+resource "aws_security_group" "web_sg" {
+  name   = "${var.project}-ec2-sg"
+  vpc_id = var.vpc_id
+ 
+  ingress {
+    description = "HTTP from VPC (ALB in same VPC will route)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # <-- prefer tighten to your VPC; adjust if you changed VPC CIDR
+  }
+ 
+  ingress {
+    description = "SSH from admin IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  tags = { Name = "${var.project}-ec2-sg" }
+}
 # Módulo compute: crea las instancias EC2 para los servidores de aplicación
 
 resource "aws_instance" "app" {
