41163751c01dc2997c02caa35f5191d49117693a
Joaquin1899 <jmirasfranchi@gmail.com>
2025-11-17 22:08:14 -0300

    ver errores

--- INFRA-MODULAR/TERRAFORM/modules/compute/user_data.sh
+++ INFRA-MODULAR/TERRAFORM/modules/compute/user_data.sh
@@ -12,9 +12,10 @@ if ! command -v aws >/dev/null 2>&1; then
   yum install -y awscli
 fi
 
-
+echo $(pwd)
 git clone https://github.com/Joaquin1899/obligatorio-test /home/ec2-user/obligatorio
 git clone https://github.com/ORT-FI-7417-SolucionesCloud/e-commerce-obligatorio-2025 /home/ec2-user/obligatorio/html
+
 cd /home/ec2-user/obligatorio
 docker build -t obligatorio .
 docker run -d -p 8080:80 --name obligatorio obligatorio
