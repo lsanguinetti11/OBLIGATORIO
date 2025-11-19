d8060dba01af21884faa3afee34f2153717c156a
Joaquin1899 <jmirasfranchi@gmail.com>
2025-11-18 23:25:47 -0300

    se llama correctamente al output

--- INFRA-MODULAR/TERRAFORM/outputs.tf
+++ INFRA-MODULAR/TERRAFORM/outputs.tf
@@ -1,35 +1,35 @@
 output "db_endpoint" {
   description = "Endpoint de la base de datos"
-  value       = module.mysql.address
+  value       = module.rds.db_endpoint
 }
 
 output "db_instance_arn" {
   description = "ARN de la instancia RDS"
-  value       = module.mysql.arn
+  value       = module.rds.db_instance_arn
 }
 
 output "db_security_group_id" {
   description = "ID del Security Group del RDS"
-  value       = module.rds.id
+  value       = module.rds.db_security_group_id
 }
 output "alb_dns" {
-  value = module.alb.dns_name
+  value = module.compute.alb_dns
 }
 
 output "asg_name" {
-  value = module.asg.name
+  value = module.compute.asg_name
 }
 
 output "alb_sg_id" {
-  value = module.alb_sg.id
+  value = module.compute.alb_sg_id
 }
 output "ec2_sg_id" {
-  value = module.ec2_sg.id
+  value = module.compute.ec2_sg_id
 }
 output "backup_vault_name" {
-  value = module.rds_backup_vault.name
+  value = module.backup.backup_vault_name
 }
 
 output "backup_plan_id" {
-  value = module.rds_backup_plan.id
+  value = module.backup.backup_plan_id
 } 
