558776c2609a34a4ee6bb7a51a44a6afdfcc269e
Luis Sanguinetti <tu_email@example.com>
2025-11-21 15:53:16 -0300

    se ajustan valores de alarmas

--- INFRA-MODULAR/TERRAFORM/modules/monitoring/main.tf
+++ INFRA-MODULAR/TERRAFORM/modules/monitoring/main.tf
@@ -8,10 +8,10 @@ resource "aws_sns_topic" "alerts" {
 resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
   alarm_name          = "${var.project}-rds-cpu-high"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
+  evaluation_periods  = 1
   metric_name         = "CPUUtilization"
   namespace           = "AWS/RDS"
-  period              = 300
+  period              = 60
   statistic           = "Average"
   threshold           = 70
   alarm_description   = "Alarma cuando CPU > 70% en RDS"
@@ -26,10 +26,10 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
 resource "aws_cloudwatch_metric_alarm" "rds_storage_low" {
   alarm_name          = "${var.project}-rds-storage-low"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 2
+  evaluation_periods  = 1
   metric_name         = "FreeStorageSpace"
   namespace           = "AWS/RDS"
-  period              = 300
+  period              = 60
   statistic           = "Average"
   threshold           = 2000000000 # ~2 GB
   alarm_description   = "Alarma cuando espacio libre < 2GB en RDS"
@@ -44,10 +44,10 @@ resource "aws_cloudwatch_metric_alarm" "rds_storage_low" {
 resource "aws_cloudwatch_metric_alarm" "rds_connections_high" {
   alarm_name          = "${var.project}-rds-connections-high"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
+  evaluation_periods  = 1
   metric_name         = "DatabaseConnections"
   namespace           = "AWS/RDS"
-  period              = 300
+  period              = 60
   statistic           = "Average"
   threshold           = 50
   alarm_description   = "Alarma cuando conexiones > 50 en RDS"
